597a1807fbd5a5ba3ac90e068e172d8992c85542
Ktastr0phe <neonox278@gmail.com>
2026-09-11 16:09:19 +0200

    Modification des tests unitaires de Add

--- tp1git/src/test/java/iut/AddTest.java
+++ tp1git/src/test/java/iut/AddTest.java
@@ -8,6 +8,11 @@ public class AddTest {
     @Test
     public void testAdd() {
         Add adder = new Add();
-        assertEquals(5, adder.add(2, 3), "2 + 3 doit être égal à 5");
+        assertEquals(7, adder.add(1, 6), "1 + 6 doit être égal à 7");
+    }
+    @Test
+    public void testAdd2() {
+        Add adder = new Add();
+        assertEquals(10, adder.add(5, 5), "5 + 5 doit être égal à 10");
     }
 }
