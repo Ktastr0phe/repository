b8fe683f021a95115ae027cc7c5fdf95cc511ce0
Ktastr0phe <neonox278@gmail.com>
2026-09-11 16:40:34 +0200

    Ajout addition

--- tp1git/src/main/java/iut/App.java
+++ tp1git/src/main/java/iut/App.java
@@ -6,5 +6,6 @@ package iut;
 public class App {
     public static void main(String[] args) {
         System.out.println("Calculatrice");
+        System.out.println("Addition fonctionnelle dans la calculatrice");
     }
 }
