83d13d0c24dacb541923068cd58ab2982210cc06
Etudiant <Etudiant@INFO-B009-PC06.crie.i-univ-tlse2.fr>
2026-09-11 13:49:21 +0200

    Ajout de la soustraction

--- tp1git/src/test/java/iut/AppTest.java
+++ tp1git/src/test/java/iut/AppTest.java
@@ -1,19 +1,15 @@
 package iut;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
 
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void testAmoinsB() {
+        Sub sub = new Sub();
+
+        assertEquals(1, sub.sub(9, 8));
     }
 }
